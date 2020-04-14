5a684c0ab92d653f692cf9e993c2a061cbe09025
clliao <yorwxue@gmail.com>
2020-04-14 14:15:06 +0800

    save log of training procedure

--- utils/loss.py
+++ utils/loss.py
@@ -1,4 +1,7 @@
 import tensorflow as tf
+import os
+import numpy as np
+import cv2
 
 
 def ohem(loss, fg_mask, bg_mask, negative_ratio=3.):
@@ -28,6 +31,31 @@ def batch_ohem(loss, fg_mask, bg_mask, negative_ratio=3.):
     return tf.map_fn(lambda x: ohem(x[0], x[1], x[2], negative_ratio), elems=[loss, fg_mask, bg_mask], dtype=tf.float32)
 
 
+def save_log(preds, losses, gts, fg_masks, bg_masks, type, prefix):
+    idx = 0
+    for pred, loss, gt, fg_mask, bg_mask in zip(preds, losses, gts, fg_masks, bg_masks):
+        pred = np.asarray(pred)
+        loss = np.asarray(loss)
+        gt = np.asarray(gt)
+        fg_mask = np.asarray(fg_mask)
+        bg_mask = np.asarray(bg_mask)
+        pred_norm = (pred - np.min(pred)) / (np.max(pred) - np.min(pred))
+        loss_norm = loss  # (loss - np.min(loss)) / (np.max(loss) - np.min(loss))
+        train_mask = fg_mask + bg_mask
+        img_pred = np.transpose([pred_norm, pred_norm, pred_norm], (1, 2, 0)) * 255
+        img_loss = np.transpose([loss_norm, loss_norm, loss_norm], (1, 2, 0)) * 255
+        img_gt = np.transpose([gt, gt, gt], (1, 2, 0)) * 255
+        img_mask = np.transpose([train_mask, train_mask, train_mask], (1, 2, 0)) * 255
+        if not os.path.exists("logs"):
+            os.makedirs("logs")
+        cv2.imwrite("./logs/%s_%d_%s_img_pred.jpg" % (prefix, idx, type), img_pred)
+        cv2.imwrite("./logs/%s_%d_%s_loss.jpg" % (prefix, idx, type), img_loss)
+        cv2.imwrite("./logs/%s_%d_%s_gt.jpg" % (prefix, idx, type), img_gt)
+        cv2.imwrite("./logs/%s_%d_%s_masked.jpg" % (prefix, idx, type), img_mask)
+        idx += 1
+    print("%s log saved." % type)
+
+
 class craft_mse_loss(tf.keras.Model):
     def __init__(self):
         super(craft_mse_loss, self).__init__()
@@ -35,7 +63,11 @@ class craft_mse_loss(tf.keras.Model):
 
     def call(self, args):
         region_true, affinity_true, region_pred, affinity_pred, confidence, fg_mask, bg_mask = args
-
+        """
+        temp = np.asarray(region_true)[0]
+        img_temp = np.transpose([temp, temp, temp], (1, 2, 0)) * 255
+        cv2.imwrite("./logs/temp.jpg", img_temp)
+        """
         confidence_mask = tf.greater_equal(confidence, tf.constant(self.confidence_threshold, dtype=tf.float32))
         confidence = tf.where(confidence_mask, confidence, tf.zeros_like(confidence))
 
@@ -50,8 +82,14 @@ class craft_mse_loss(tf.keras.Model):
         train_mask = hard_bg_mask + fg_mask
         l_total = l_total * train_mask
 
-        return tf.reduce_sum(l_total) / (tf.reduce_sum(confidence * train_mask) + tf.keras.backend.epsilon())
+        # show loss map
+        """
+        save_log(region_pred[0], l_region[0], region_true[0], fg_mask[0], hard_bg_mask[0], "region", "0")
+        save_log(affinity_pred[0], l_affinity[0], affinity_true[0], fg_mask[0], hard_bg_mask[0], "affinity", "0")
+        # """
 
+        loss = tf.reduce_sum(l_total) / (tf.reduce_sum(confidence * train_mask) + tf.keras.backend.epsilon())
+        return loss, l_region, l_affinity, hard_bg_mask
 
 class craft_mae_loss(tf.keras.Model):
     def __init__(self):
